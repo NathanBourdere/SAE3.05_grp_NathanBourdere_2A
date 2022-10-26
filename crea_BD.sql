a61f81d9d0bba024bda62c9b04accf0afdda0b04
BOURDERE_ANDREOU Nathan <o22100472@info03-04.iut45.univ-orleans.fr>
2022-10-26 12:38:20 +0200

    ajout fonction trigger dateAssignationPassee

--- crea_BD.sql
+++ crea_BD.sql
@@ -8,11 +8,11 @@ DROP TABLE COURS;
 
 CREATE TABLE UTILISATEUR(
     IDuser varchar(20) NOT NULL,
-    nomU varchar(20),
-    prenomU varchar(20),
+    nomU varchar(30),
+    prenomU varchar(30),
     telU int(14),
     ddnU DATE,
-    mail varchar(20),
+    mail varchar(50),
     PRIMARY KEY(IDuser)
 );
 
@@ -43,7 +43,7 @@ CREATE TABLE COURS(
     TypeCours varchar(2) NOT NULL,
     nomCours varchar(42),
     domaine varchar(42),
-    heuresTotale int(3) check (heuresTotale between 0 and 23),
+    heuresTotale int(3),
     dureeCours int(3),
     PRIMARY KEY(IDCours,TypeCours)
 );
@@ -62,6 +62,8 @@ CREATE TABLE ASSIGNER(
     IDvacataire varchar(20) NOT NULL,
     salle varchar(12),
     classe varchar(4),
+    dateCours DATE,
+    heureCours int(2) check (heuresCours between 0 and 23),
     PRIMARY KEY (IDCours,TypeCours,IDvacataire)
 );
 
@@ -82,7 +84,7 @@ create or replace trigger ancienPasAssignable before insert on ASSIGNER for each
 BEGIN
     declare res varchar(500) DEFAULT '';
     declare est_ancien boolean DEFAULT FALSE;
-    select ancien into est_ancien from ASSIGNER NATURAL JOIN VACATAIRE where IDvacataire = new.IDvacataire;
+    select ancien into est_ancien from ASSIGNER NATURAL JOIN VACATAIRE where IDvacataire = new.IDvacataire and IDCours = new.IDCours and TypeCours = new.TypeCours;
     if (est_ancien = TRUE) then
         set res = concat(res,"erreur : ",new.IDvacataire," est un ancien vacataire, il n'enseigne plus ici, assignation impossible");
         signal SQLSTATE '45000' set MESSAGE_TEXT = res;
@@ -94,9 +96,9 @@ DELIMITER ;
 DELIMITER |
 create or replace trigger vacataireNonAssignableTantQuePasValidee before insert on ASSIGNER for each row
 BEGIN
-    declare res varchar(500) DEFAULt '';
+    declare res varchar(500) DEFAULT '';
     declare etat_doc varchar(20) DEFAULT '';
-    select etat_dossier into etat_doc from ASSIGNER NATURAL JOIN VACATAIRE NATURAL JOIN GERERDOSSIER where IDvacataire = new.IDvacataire;
+    select etat_dossier into etat_doc from ASSIGNER NATURAL JOIN VACATAIRE NATURAL JOIN GERERDOSSIER where IDvacataire = new.IDvacataire  and IDCours = new.IDCours and TypeCours = new.TypeCours;
     if (etat_doc != "Validé") then
         set res = concat(res,"erreur : ",new.IDvacataire," à son dossier laissé en l'état : ",etat_doc, ", son dossier doit être validé avant");
         signal SQLSTATE '45000' set MESSAGE_TEXT = res;
@@ -108,11 +110,26 @@ DELIMITER ;
 DELIMITER |
 create or replace trigger vacataireNonAffectable before insert on ASSIGNER for each row
 BEGIN
-    declare res varchar(500) DEFAULt '';
+    declare res varchar(500) DEFAULT '';
     declare idv_dans_affectable varchar(20) DEFAULT '';
     select IDvacataire into idv_dans_affectable from AFFECTABLE where IDvacataire = new.IDvacataire and IDCours = new.IDCours and TypeCours = new.TypeCours;
     if (idv_dans_affectable != '') then
         set res = concat(res,"erreur : ",idv_dans_affectable," n'est pas affectable au cours ",new.IDCours,"(en ",new.TypeCours,")");
+        signal SQLSTATE '45000' set MESSAGE_TEXT = res;
+    end if;
+END |
+DELIMITER ;
+
+--trigger numéro 4 : Un vacataire ne peut pas se faire assigner un cours (et son type) si la date du cours est du passé
+DELIMITER |
+create or replace trigger dateAssignationPassee before insert on ASSIGNER for each row
+BEGIN
+    declare res varchar(500) DEFAULt '';
+    declare idv_dans_affectable varchar(20) DEFAULT '';
+    declare la_date DATE;
+    select date_cours into la_date from ASSIGNER where IDvacataire = new.IDvacataire and IDCours = new.IDCours and TypeCours = new.TypeCours;
+    if (la_date < CURDATE()) then
+        set res = concat(res,"erreur : ",idv_dans_affectable," n'est pas affectable au cours car la date indiquée est passée");
         signal SQLSTATE '45000' set MESSAGE_TEXT = res;
     end if;
 END |
