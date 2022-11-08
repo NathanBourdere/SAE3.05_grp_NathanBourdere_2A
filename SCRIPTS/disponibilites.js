4005dcf6caa301b0e060ac3004473b42d7b86d05
BryanBlinDorard <brybrybbd@mail.com>
2022-11-08 17:09:20 +0100

    ajout de la verification des valeurs pour la disponibilité

--- SCRIPTS/disponibilites.js
+++ SCRIPTS/disponibilites.js
@@ -17,3 +17,30 @@ function ajouterDispoDate() {
     ul.innerHTML = "<li><input type=\"date\"></li><li>de</li><li><select name=\"heure_debut\"><option value=\"\">Heure de début</option><option value=\"8\">8h</option><option value=\"9\">9h</option><option value=\"10\">10h</option><option value=\"11\">11h</option><option value=\"12\">12h</option><option value=\"13\">13h</option><option value=\"14\">14h</option><option value=\"15\">15h</option><option value=\"16\">16h</option><option value=\"17\">17h</option><option value=\"18\">18h</option></select></li><li>à</li><li><select name=\"heure_fin\"><option value=\"\">Heure de fin</option><option value=\"8\">8h</option><option value=\"9\">9h</option><option value=\"10\">10h</option><option value=\"11\">11h</option><option value=\"12\">12h</option><option value=\"13\">13h</option><option value=\"14\">14h</option><option value=\"15\">15h</option><option value=\"16\">16h</option><option value=\"17\">17h</option><option value=\"18\">18h</option></select></li><li><button onclick=\"supprimerDispo()\">Supprimer</button></li>"
     datesSpe.appendChild(ul);
 }
+
+function verifier_valeur() {
+    const periodes = document.getElementById("periodes");
+    let verif = false
+    for (let i = 0; i < periodes.children.length; i++) {
+        const ul = periodes.children[i];
+        for (let j = 0; j < ul.children.length; j++) {
+            const li = ul.children[j];
+            if (li.children.length>0){
+                for (let k = 0; k < li.children.length; k++) {
+                    const select = li.children[k]; 
+                    if (select.tagName == "SELECT") {
+                        if (select.value == "") {
+                            verif = true;
+                            select.style.backgroundColor = "red";
+                        } else {
+                            select.style.backgroundColor = "white";
+                        }
+                    }
+                }
+            }
+        }
+    }
+    if (verif) {
+        alert("Veuillez remplir tous les champs");
+    }
+}
