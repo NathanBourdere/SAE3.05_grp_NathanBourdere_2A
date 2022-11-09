79f11c74c7af0d1bcaf9f42468e2a29b915a5fc9
Constantin LOSZACH <constantinlozach@gmail.com>
2022-11-09 10:10:41 +0100

    Implémentation de la suppression de blocs.

--- SCRIPTS/disponibilites.js
+++ SCRIPTS/disponibilites.js
@@ -1,20 +1,55 @@
-function supprimerDispo() {
-    window.confirm("Êtes-vous sûr ?");
+function supprimerDispo(idBouton) {
+    let div;
+    let ulBouton;
+
+    if (window.confirm("Êtes-vous sûr ?")) {
+        if (idBouton.slice(0,2) === "BP") {
+            div = document.getElementById("periodes");
+        }else{
+            div = document.getElementById("dates_spe");
+        }
+    }
+    for (let enfant of div.children) {
+        if (enfant.children[enfant.children.length-1].children[0].id === idBouton) {
+            ulBouton = enfant;
+            break;
+        }
+    }
+    ulBouton.remove();
 }
 
-function ajouterDispoPeriode() {
+function ajouterDispoPeriode() { 
     const periodes = document.getElementById("periodes");
     const ul = document.createElement("ul");
+    let idBoutonDernierUL;
 
-    ul.innerHTML = "<li><select name=\"jour_semaine\"><option value=\"\">Jour de la semaine</option><option value=\"lundi\">Lundi</option><option value=\"mardi\">Mardi</option><option value=\"mercredi\">Mercredi</option><option value=\"jeudi\">Jeudi</option><option value=\"vendredi\">Vendredi</option><option value=\"samedi\">Samedi</option></select></li><li>de</li><li><select name=\"heure_debut\"><option value=\"\">Heure de début</option><option value=\"8\">8h</option><option value=\"9\">9h</option><option value=\"10\">10h</option><option value=\"11\">11h</option><option value=\"12\">12h</option><option value=\"13\">13h</option><option value=\"14\">14h</option><option value=\"15\">15h</option><option value=\"16\">16h</option><option value=\"17\">17h</option><option value=\"18\">18h</option></select></li><li>à</li><li><select name=\"heure_fin\"><option value=\"\">Heure de fin</option><option value=\"8\">8h</option><option value=\"9\">9h</option><option value=\"10\">10h</option><option value=\"11\">11h</option><option value=\"12\">12h</option><option value=\"13\">13h</option><option value=\"14\">14h</option><option value=\"15\">15h</option><option value=\"16\">16h</option><option value=\"17\">17h</option><option value=\"18\">18h</option></select></li><li><button onclick=\"supprimerDispo()\">Supprimer</button></li>"
+    if (periodes.children.length > 0) {
+        const dernierUL = periodes.children[periodes.children.length-1];
+        const boutonDernierUL = dernierUL.children[dernierUL.children.length-1].children[0];
+        idBoutonDernierUL = boutonDernierUL.id;
+    }else {
+        idBoutonDernierUL = "BP0";
+    }
+
+    ul.innerHTML = `<li><select name="jour_semaine"><option value="">Jour de la semaine</option><option value="lundi">Lundi</option><option value="mardi">Mardi</option><option value="mercredi">Mercredi</option><option value="jeudi">Jeudi</option><option value="vendredi">Vendredi</option><option value="samedi">Samedi</option></select></li><li>de</li><li><select name="heure_debut"><option value="">Heure de début</option><option value="8">8h</option><option value="9">9h</option><option value="10">10h</option><option value="11">11h</option><option value="12">12h</option><option value="13">13h</option><option value="14">14h</option><option value="15">15h</option><option value="16">16h</option><option value="17">17h</option><option value="18">18h</option></select></li><li>à</li><li><select name="heure_fin"><option value="">Heure de fin</option><option value="8">8h</option><option value="9">9h</option><option value="10">10h</option><option value="11">11h</option><option value="12">12h</option><option value="13">13h</option><option value="14">14h</option><option value="15">15h</option><option value="16">16h</option><option value="17">17h</option><option value="18">18h</option></select></li><li><button id="BP${parseInt(idBoutonDernierUL.slice(-1))+1}" onclick="supprimerDispo('BP${parseInt(idBoutonDernierUL.slice(-1))+1}')">Supprimer</button></li>`
     periodes.appendChild(ul);
+    
 }
 
 function ajouterDispoDate() {
     const datesSpe = document.getElementById("dates_spe");
     const ul = document.createElement("ul");
+    let idBoutonDernierUL;
 
-    ul.innerHTML = "<li><input type=\"date\"></li><li>de</li><li><select name=\"heure_debut\"><option value=\"\">Heure de début</option><option value=\"8\">8h</option><option value=\"9\">9h</option><option value=\"10\">10h</option><option value=\"11\">11h</option><option value=\"12\">12h</option><option value=\"13\">13h</option><option value=\"14\">14h</option><option value=\"15\">15h</option><option value=\"16\">16h</option><option value=\"17\">17h</option><option value=\"18\">18h</option></select></li><li>à</li><li><select name=\"heure_fin\"><option value=\"\">Heure de fin</option><option value=\"8\">8h</option><option value=\"9\">9h</option><option value=\"10\">10h</option><option value=\"11\">11h</option><option value=\"12\">12h</option><option value=\"13\">13h</option><option value=\"14\">14h</option><option value=\"15\">15h</option><option value=\"16\">16h</option><option value=\"17\">17h</option><option value=\"18\">18h</option></select></li><li><button onclick=\"supprimerDispo()\">Supprimer</button></li>"
+    if (datesSpe.children.length !== 0) {
+        const dernierUL = datesSpe.children[datesSpe.children.length-1];
+        const boutonDernierUL = dernierUL.children[dernierUL.children.length-1].children[0];
+        idBoutonDernierUL = boutonDernierUL.id;
+    }else{
+        idBoutonDernierUL = "BD0";
+    }
+
+    ul.innerHTML = `<li><input type="date"></li><li>de</li><li><select name="heure_debut"><option value="">Heure de début</option><option value="8">8h</option><option value="9">9h</option><option value="10">10h</option><option value="11">11h</option><option value="12">12h</option><option value="13">13h</option><option value="14">14h</option><option value="15">15h</option><option value="16">16h</option><option value="17">17h</option><option value="18">18h</option></select></li><li>à</li><li><select name="heure_fin"><option value="">Heure de fin</option><option value="8">8h</option><option value="9">9h</option><option value="10">10h</option><option value="11">11h</option><option value="12">12h</option><option value="13">13h</option><option value="14">14h</option><option value="15">15h</option><option value="16">16h</option><option value="17">17h</option><option value="18">18h</option></select></li><li><button id="BD${parseInt(idBoutonDernierUL.slice(-1))+1}" onclick="supprimerDispo('BD${parseInt(idBoutonDernierUL.slice(-1))+1}')">Supprimer</button></li>`
     datesSpe.appendChild(ul);
 }
 
